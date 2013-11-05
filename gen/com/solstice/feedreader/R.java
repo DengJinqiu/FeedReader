49e6a32f2720f61e5a46afb792849a9013f07506
jinqiu <jinqiu@jinqiu-PC.(none)>
2013-11-04 23:21:38 -0500

    Able to connect net

--- gen/com/solstice/feedreader/R.java
+++ gen/com/solstice/feedreader/R.java
@@ -24,24 +24,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
+        public static final int refresh=0x7f080004;
         public static final int section_label=0x7f080002;
+        public static final int settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main_dummy=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int mainmenu=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int cellular_connected=0x7f05000b;
+        public static final int connection_error=0x7f05000c;
+        public static final int lost_connection=0x7f050009;
+        /**  NetworkActivity 
+         */
+        public static final int page_title=0x7f050007;
+        public static final int refresh=0x7f050006;
+        /**  Menu items 
+         */
+        public static final int settings=0x7f050005;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int title_section3=0x7f050004;
+        public static final int updated=0x7f050008;
+        public static final int wifi_connected=0x7f05000a;
+        public static final int xml_error=0x7f05000d;
     }
     public static final class style {
         /** 
